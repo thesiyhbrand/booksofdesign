b9b7d580ec942751bd86e76547c6d9db0e44d126
Amarjeet Chaudhary <siyhcompany@gmail.com>
2023-07-14 15:22:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var tl = gsap.timeline({scrollTrigger:{
+    trigger:"#main",
+   //  markers:true,
+    start:"50% 50%",
+    end:"150% 50%",
+    scrub:2,
+    pin:true
+}});
+tl
+.to("#center",{
+   height: "100vh",
+},'a')
+.to("#top",{
+    top: "-50%",
+ },'a')
+ .to("#bottom",{
+    bottom: "-50%",
+ },'a')
+.to("#top-h1",{
+    top: "60%"
+ },'a')
+ .to("#bottom-h1",{
+    bottom: "-30%"
+ },'a')
+.to("#center-h1",{
+   top: "-30%"
+},'a')
+.to(".content",{
+   delay: -0.2,
+   marginTop: "0%"
+})
